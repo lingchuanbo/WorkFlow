abf327fcbf59b02694f596b4ee656d61bbe7a99e
BoBO <lingchuanbo>
2020-02-26 17:52:00 +0800

    新增便捷渲染打开方式

---
+++ custom/ae_scripts/commands/BoBO_OpenLocalFilesRender.jsx
@@ -0,0 +1,25 @@
+﻿var myProject = app.project;
+var myNumItemsIndex = myProject.renderQueue.numItems;
+if (myNumItemsIndex >= 1) {
+    var myRQItem = myProject.renderQueue.item(myNumItemsIndex);
+    var myRQFile = myRQItem.outputModule(1).file;
+    var myFileName = myProject.file.name;
+    var str=decodeURI(myRQFile);
+    // \替换/
+    var getAllDir=str.replace(/\//g,"\\"); 
+    //分割字符串
+    var getDir=getAllDir.substr(1,1);
+    //获取盘符
+    var getDirPath=getAllDir.substring(2);
+    //删除末尾字符串
+    
+    var ipos = getDirPath.lastIndexOf("\\");
+    var delString=getDirPath.substring(ipos);
+    var dirPath=getDirPath.replace(delString,"")
+
+    //完整路经
+    var Dir=getDir + ':' + dirPath +"\\"
+    system.callSystem("Explorer /select," + Dir+"\"");
+}else{
+    alert("当前无渲染文件！")
+}
---
+++ custom/ae_scripts/commands/BoBO_OpenLocalFilesRenderTC.jsx
@@ -0,0 +1,22 @@
+﻿var myProject = app.project;
+var myNumItemsIndex = myProject.renderQueue.numItems;
+if (myNumItemsIndex >= 1) {
+    var myRQItem = myProject.renderQueue.item(myNumItemsIndex);
+    var myRQFile = myRQItem.outputModule(1).file;
+    var myFileName = myProject.file.name;
+    var str=decodeURI(myRQFile);
+    // \替换/
+    var getAllDir=str.replace(/\//g,"\\"); 
+    //分割字符串
+    //
+    var getDir=getAllDir.substr(1,1);
+    //获取盘符
+    var getDirPath=getAllDir.substring(2);
+
+    //完整路经
+    var Dir=getDir + ':' + getDirPath
+
+    system.callSystem("F:\\BoBOProgram\\TotalCMD\\TOTALCMD.EXE /O /T /R=" + Dir);
+}else{
+    alert("当前无渲染文件！")
+}
